898706f9092ca7cee4dae04a552923647490950f
Erick Dagenais <erdagena@microsoft.com>
2021-02-24 13:44:05 -0800

    This script works. Don't use dns label for private ip

--- createcontainer.sh
+++ createcontainer.sh
@@ -1,19 +1,20 @@
 # i guess this has to be in the same sub as the storage account or else wont be able to access storage 
 # ` (backtick is used in powershell)
+# https://docs.microsoft.com/en-us/azure/container-instances/container-instances-application-gateway
+# i guess need to add application gateway to connect to container in vnet
+# so each container group needs its own vnet with peering 
+# will need to use load balance for public ip
 az container create `
 --resource-group rg-wagyu-testnet-common-westus2 `
 --cpu 2 `
---dns-name-label aci-sentry-1 `
 --image rickityd/bnode:first `
---ip-address Public `
+--ip-address private `
 --location westus2 `
 --memory 2 `
 --name aci-sentry-1-westus2 `
---vnet /subscriptions/ca72fd04-5454-42a0-aa80-1e087cdfb1cd/resourceGroups/rg-wagyu-testnet-westus2/providers/Microsoft.Network/virtualNetworks/rg-wagyu-testnet-westus2-vnet `
---subnet /subscriptions/ca72fd04-5454-42a0-aa80-1e087cdfb1cd/resourceGroups/rg-wagyu-testnet-westus2/providers/Microsoft.Network/virtualNetworks/rg-wagyu-testnet-westus2-vnet/subnets/default `
+--vnet vnet-wagyu-testnet `
+--subnet default `
 --os-type linux `
---ports [80] `
---protocol TCP `
 --restart-policy OnFailure `
 --azure-file-volume-account-key <redacted> `
 --azure-file-volume-account-name blockchainstate `
